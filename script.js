c658c47ec350ae2f89e3504dda6ef22a6a63e636
Leon Stange <leon.stange@glinicke.de>
2022-03-16 12:01:44 +0100

    DOM-Elemente ausgewählt und in Variabeln gespeichert

---
+++ script.js
@@ -0,0 +1,6 @@
+// select elements
+const theme = document.querySelector("#theme");
+const inputText = document.querySelector("#input-task");
+const todoList = document.querySelector(".todolist-container");
+const itemsLeft = document.querySelector(".item-count");
+const clearCompleted = document.querySelector(".clear-completed");
